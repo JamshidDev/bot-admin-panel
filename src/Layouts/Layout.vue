324f800a0350c9a86a8c978050af747db20bb269
JamshidDev <raximovj662@gmail.com>
2023-10-24 01:18:26 +0500

    added

--- src/Layouts/Layout.vue
+++ src/Layouts/Layout.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="layout-wrapper" :class="sidebar ? 'open-sidebar' : 'close-sidebar'">
     <div class="sidebar-container">
-    <button @click="control_sidebar">
-      control
-    </button>
+    <Sidebar></Sidebar>
     </div>
     <div class="main-page-content">
       <Toolbar @sidebar-event="control_sidebar()" />
@@ -11,29 +9,7 @@
         <router-view></router-view>
       </div>
     </div>
-
   </div>
-
-
-
-<!--    <div :class="[open_sidebar ? 'open-sdebar' : 'close-sdebar']"-->
-<!--        class="w-full min-h-screen relative overflow-hidden box-border ">-->
-<!--        <div :class="[open_sidebar ? 'left-[0px]' : 'left-[-260px]']"-->
-<!--            class="sidebar-scroll pb-6 bg-[#111827] w-[260px] overflow-y-auto   fixed z-[6]  top-0 min-h-screen max-h-screen  transition-all duration-300">-->
-<!--            <Sidebar />-->
-<!--        </div>-->
-<!--        <div class="page-content block w-full transition-all duration-300">-->
-<!--            <Toolbar @sidebar-event="control_sidebar()" />-->
-<!--        </div>-->
-<!--        <div-->
-<!--            class=" page-content block bg-[#f6f9f8]  min-h-screen  w-full relative  transition-all duration-300 px-4 ">-->
-<!--            <router-view></router-view>-->
-<!--        </div>-->
-<!--        &lt;!&ndash; sidebar overal menu &ndash;&gt;-->
-<!--        <div @click="control_sidebar()" v-if="open_sidebar"-->
-<!--            class="layout-overal_bg  w-full fixed min-h-screen z-[5] bg-color-layout-overal top-0 left-0 bottom-0 right-0">-->
-<!--        </div>-->
-<!--    </div>-->
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
--- src/Layouts/components/Sidebar.vue
+++ src/Layouts/components/Sidebar.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="w-full">
-        <div class="w-full my-4 pb-4 pt-1 flex gap-2 justify-center items-center border-b-[1px] border-[#6e7582] ">
-            <img src="../../assets/picture/robot.png" class="w-[30px]" alt="">
-            <span class="font-bold uppercase text-white text-xl">Redox bot</span>
-        </div>
-        <div v-for="item in menu_list" :key="item.to" :class="route.path == item.to && 'text-[#ffffff]'" @click="router.push(item.to)"
-            class="w-full text-[#6e7582] hover:text-white">
-            <div class="flex gap-2 mx-3 my-1 p-2 cursor-pointer transition-all duration-200">
-                <vue-feather :type="item.icon" size="18" class=""></vue-feather>
-                <div class="text-sm font-normal ">{{ item.label }}</div>
-            </div>
-        </div>
-        <a href="tel:+998977716004" class="block m-4 p-4 border text-[#5036c5] border-[#5036c5] rounded-lg mt-10 shadow-lg">
-            <h3 class=" text-center text-sm uppercase font-bold ">Support call</h3>
-            <h3 class=" text-center text-sm uppercase font-bold cursor-pointer ">
-                +998 (97) 771-60-04
-            </h3>
-            <h6 class="text-center text-xs uppercase font-normal mt-2 ">🧑‍💻 Jamshid Raximov</h6>
-        </a>
+  <div class="sidebar-dark-box">
+    <div class="logo-box">
+      <img src="../../assets/picture/robot.png" alt="">
+      <span class="logo-name" >Telegram Bot</span>
     </div>
+    <div class="item-subtitle">
+      Asosiy
+    </div>
+    <div class="single-item" v-for="item in menu_list" :key="item.to">
+      <n-icon size="26"  class="item-icon" :v-html="item.icon">
+
+      </n-icon>
+      <span>Dashboard</span>
+    </div>
+
+
+    <div class="help-center-box">
+        <span class="help-icon">
+          <n-icon size="26"  color="#6090E4">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M60.44 389.17c0 .07 0 .2-.08.38c.03-.12.05-.25.08-.38z" fill="currentColor"></path><path d="M439.9 405.6a26.77 26.77 0 0 1-9.59-2l-56.78-20.13l-.42-.17a9.88 9.88 0 0 0-3.91-.76a10.32 10.32 0 0 0-3.62.66c-1.38.52-13.81 5.19-26.85 8.77c-7.07 1.94-31.68 8.27-51.43 8.27c-50.48 0-97.68-19.4-132.89-54.63A183.38 183.38 0 0 1 100.3 215.1a175.9 175.9 0 0 1 4.06-37.58c8.79-40.62 32.07-77.57 65.55-104A194.76 194.76 0 0 1 290.3 32c52.21 0 100.86 20 137 56.18c34.16 34.27 52.88 79.33 52.73 126.87a177.86 177.86 0 0 1-30.3 99.15l-.19.28l-.74 1c-.17.23-.34.45-.5.68l-.15.27a21.63 21.63 0 0 0-1.08 2.09l15.74 55.94a26.42 26.42 0 0 1 1.12 7.11a24 24 0 0 1-24.03 24.03z" fill="currentColor"></path><path d="M299.87 425.39a15.74 15.74 0 0 0-10.29-8.1c-5.78-1.53-12.52-1.27-17.67-1.65a201.78 201.78 0 0 1-128.82-58.75A199.21 199.21 0 0 1 86.4 244.16C85 234.42 85 232 85 232a16 16 0 0 0-28-10.58s-7.88 8.58-11.6 17.19a162.09 162.09 0 0 0 11 150.06C59 393 59 395 58.42 399.5c-2.73 14.11-7.51 39-10 51.91a24 24 0 0 0 8 22.92l.46.39A24.34 24.34 0 0 0 72 480a23.42 23.42 0 0 0 9-1.79l53.51-20.65a8.05 8.05 0 0 1 5.72 0c21.07 7.84 43 12 63.78 12a176 176 0 0 0 74.91-16.66c5.46-2.56 14-5.34 19-11.12a15 15 0 0 0 1.95-16.39z" fill="currentColor"></path></svg>
+          </n-icon>
+        </span>
+      <div class="help-text">
+        <span>Support call</span>
+        <span>+998(97)7716004</span>
+      </div>
+      <span class="help-icon">
+          <n-icon size="20"  color="#83899F">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16"><g fill="none"><path d="M5.74 3.2a.75.75 0 0 0-.04 1.06L9.227 8L5.7 11.74a.75.75 0 1 0 1.1 1.02l4-4.25a.75.75 0 0 0 0-1.02l-4-4.25a.75.75 0 0 0-1.06-.04z" fill="currentColor"></path></g></svg>
+          </n-icon>
+        </span>
+    </div>
+
+
+
+
+  </div>
+
 </template>
  
 <script setup>
@@ -36,7 +54,10 @@ const route = useRoute();
 const menu_list = [
     {
         label: 'Umumiy Statistika',
-        icon: 'pie-chart',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 496 512">
+<path d="M248 8C111 8 0 119 0 256s111 248 248 248s248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88s-88-39.4-88-88s39.4-88
+ 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2c18.8-35.4 55.6-59.8 98.5-59.8c2.4 0 4.8.4 7.1 1.1c13 4.2 26.6 6.9 40.9 6.9c14.3 0 28-2.7 40.9-6.9c2.3-.7
+  4.7-1.1 7.1-1.1c42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z" fill="currentColor"></path></svg>`,
         to: '/coming-soon',
         visible: () => true,
 
